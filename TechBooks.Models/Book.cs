44be034a615e97d860ef3311d590b292394791d0
Maqbul Pasha <160996102+Maqbul-Pasha@users.noreply.github.com>
2024-10-08 22:31:09 -0400

    first commit

---
+++ TechBooks.Models/Book.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace TechBooks.Models;
+
+public class Book
+{
+    [Key]
+    [DisplayName("Book Id")]
+    public int BookId { get; set; }
+
+    [Required]
+    [DisplayName("Category")]
+    public int CategoryId { get; set; }
+
+    public Category Category { get; set; } = new Category();
+
+    [Required]
+    [StringLength(50)]
+    public string Title { get; set; } = string.Empty;
+
+    [Required]
+    public DateTime CreationDate { get; set; }
+
+    public List<Author> Authors { get; set; } = new List<Author>();
+}
